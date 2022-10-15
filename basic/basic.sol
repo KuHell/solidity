776e549880ee23bf54b85ff2626a4ab2a53493bc
KuHell <itkdgus489@gmail.com>
2022-10-15 22:39:42 +0900

    test

--- basic/basic.sol
+++ basic/basic.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract basic {
-    uint256 test_num = 5;
+    uint256 test_num = 6;
 
     function call_num() public view returns (uint256) {
         return test_num;
